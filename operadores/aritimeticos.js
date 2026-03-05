d36f896babb59118a2e88c7581627fed2fb94386
Plima01 <pedrohenrique061377@gmail.com>
2026-03-05 12:10:24 -0300

    aritimeticos em JS

---
+++ operadores/aritimeticos.js
@@ -0,0 +1,17 @@
+var numeros = 10
+// valores literais 
+console.log(10 + 5); // adição
+console.log(10 - 5); // subtração
+console.log(10 * 5); // multiplicaçao
+console.log(10 / 5); // divisao
+console.log(10 % 5); // resto da divisão
+
+// valores variaveis 
+let a = 100 // let é
+let b = 50
+
+console.log(a + b); // adição
+console.log(a - b); // subtração
+console.log(a * b); // multiplicaçao
+console.log(a / b); // divisao
+console.log(a % b); // resto da divisão 
