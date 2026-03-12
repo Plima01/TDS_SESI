be76e557d257f5527e1e0bb7b18691d4eaa89af0
Plima01 <pedrohenrique061377gmail.com>
2026-03-12 12:12:40 -0300

    Estruturas de controle

---
+++ controle/else_if.js
@@ -0,0 +1,29 @@
+// desafio fazer comparacao de notas de alunos, aprovado reprovado e em recupração 
+/*let nota = 8.5;
+const media = 7.5
+
+if(nota >= media){
+    console.log("Aprovado");    
+}
+else if(nota >= 5){
+    console.log("Recuperação"); 
+}
+else{
+    console.log("Reprovado");
+}
+*/
+
+let n1 = 10;
+let n2 = 5;
+let n3 = 8;
+let media = (n1 + n2 + n3) / 3;
+
+if(media >= 7.5){
+    console.log("Aprovado");    
+}
+else if(media >= 5){
+    console.log("Recuperação"); 
+}
+else{
+    console.log("Reprovado");
+}
---
+++ controle/tenplate_string.js
@@ -0,0 +1,5 @@
+let nome = "lucas rodrigo";
+let idade = 24;
+//imrpimir no terminal o nome e a idade juntp com uma frase 
+console.log("Meu nome é " + nome + " e minha idade é " + idade +" anos");
+console.log(`Meu nome é ${nome} e minha idade é ${idade} anos`);
